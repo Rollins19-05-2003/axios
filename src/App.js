fa7400e7a90118f33430e10fb38f1df9d70d6665
Rollins19-05-2003 <sdeb7433@gmail.com>
2024-08-31 12:07:58 +0530

    axios practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useState, useEffect} from "react";
 
-function App() {
+const App = () => {
+  const [myData, setMyData] = useState([]);
+  const [myError, setMyError] = useState([]);
+
+  const fetchData = async() =>{
+    try{
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setMyData(res.data);
+    }catch(error){
+      setMyError(error.message);
+    }
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-500">
+      <h1 className="text-white text-center font-semibold p-4">This page will contain the data from fake API</h1>
+      {myError !== "" && <h2 className="text-red text-3xl font-extralight">{myError}</h2>}
+
+      <div className="flex overflow-x-scroll">
+        {myData.slice(0,15).map((data)=>{
+          const {id, body, title} = data;
+          return(
+            <div key={id} className="border border-black h-fit p-4 m-4">
+              <h1 className="font-bold ">Title : {title.slice(0,25)}</h1>
+              <h1 className="text-white">{body.slice(0,150)}</h1>
+            </div>
+          )
+        })};
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
